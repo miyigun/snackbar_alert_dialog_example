df6706f2e41571d5277f96e4e7640f7bf2ca00ce
Murat Iyigun <miyigun@hotmail.com>
2023-11-16 20:59:33 +0300

    Commit 3

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -9,16 +9,193 @@ const SnackBar snackBarOne=SnackBar(
   ),
 );
 
+const SnackBar snackBarTwo=SnackBar(
+  content: Text(
+    'Search has been pressed!',
+    style: TextStyle(fontSize: 30),
+  ),
+);
+
+const SnackBar snackBarThree=SnackBar(
+  content: Text(
+    'Navigation has been pressed!',
+    style: TextStyle(fontSize: 30),
+  ),
+);
+
+void clickNextPage(BuildContext context){
+  Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (BuildContext context){
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Know yourself...'),
+              ),
+              body: const Center(
+                child: Text(
+                  'Dig deep into every layer of your mind to find yourself...',
+                  style: TextStyle(
+                    fontSize: 34,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          }
+      )
+  );
+}
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text(''),
+      key: scaffoldKey,
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: (){
+                ScaffoldMessenger.of(context).showSnackBar(snackBarOne);
+              },
+              icon: const Icon(Icons.add_alert),
+            tooltip: 'Show SnackBar',
+          ),
+          IconButton(
+            onPressed: (){
+              ScaffoldMessenger.of(context).showSnackBar(snackBarTwo);
+            },
+            icon: const Icon(Icons.search),
+            tooltip: 'Search',
+          ),
+          IconButton(
+            onPressed: (){
+              clickNextPage(context);
+            },
+            icon: const Icon(Icons.navigate_next),
+            tooltip: 'Search',
+          ),
+        ],
+        leading: IconButton(
+          onPressed: (){
+            ScaffoldMessenger.of(context).showSnackBar(snackBarThree);
+          },
+          icon: const Icon(Icons.menu),
+          tooltip: 'Navigation Menu',
+        ),
+        title: const Text(
+          'Knowledge Test',
+          style: TextStyle(
+            fontSize: 25,
+            fontStyle: FontStyle.normal,
+          ),
+        ),
+        backgroundColor: Colors.redAccent,
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text(
+              'Answer a few questions and know your level...',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 15,
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 1,
+              ),
+            ),
+            padding: const EdgeInsets.all(10),
+            width: 350,
+            child: Card(
+              elevation: 20,
+              color: Colors.red,
+              child: ElevatedButton(
+                child: const Text(
+                  'You have chosen answer 1',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: (){
+
+                },
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 15,
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 1,
+              ),
+            ),
+            padding: const EdgeInsets.all(10),
+            width: 350,
+            child: Card(
+              elevation: 20,
+              color: Colors.red,
+              child: ElevatedButton(
+                child: const Text(
+                  'You have chosen answer 2',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: (){
+
+                },
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 15,
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 1,
+              ),
+            ),
+            padding: const EdgeInsets.all(10),
+            width: 350,
+            child: Card(
+              elevation: 20,
+              color: Colors.red,
+              child: ElevatedButton(
+                child: const Text(
+                  'You have chosen answer 3',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: (){
+
+                },
+              ),
+            ),
+          ),
+        ],
+      )
 
     );
   }
